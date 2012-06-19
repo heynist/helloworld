ab1a76cf2f0c5b6321d13949cd5f5a3e7a718cf0
Steven Heyninck <steven.heyninck@cronos.be>
2012-06-19 17:50:29 +0200

    nimble async read

---
+++ nimble_async_read.js
@@ -0,0 +1,24 @@
+var nimble = require('nimble');
+
+function eofCallback(err, data) {
+    if (err)
+        console.error ("error encountered:" + err);
+    else
+        console.log('eofCallback called: file has ' + data.length + ' characters');
+}
+
+var fs=require('fs');
+
+nimble.parallel([
+    function(callback) {
+        var content = fs.readFile('./hello.js','utf-8', eofCallback);
+    },
+    function(callback) {
+        var content = fs.readFile('./converter.js','utf-8', eofCallback);
+    },
+    function(callback) {
+        var content = fs.readFile('./closure.js','utf-8', eofCallback);
+    }], function (callback) {
+        console.log('all done');
+    }
+);
